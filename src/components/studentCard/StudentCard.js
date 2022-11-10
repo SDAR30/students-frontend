6b94757ce9233ae8199f3b110f80cdc0d7021e51
Shoaib Dar <SDAR30@gmail.com>
2022-11-10 13:29:41 -0500

    7/13 update complete

--- src/components/studentCard/StudentCard.js
+++ src/components/studentCard/StudentCard.js
@@ -1,23 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import { FaPlus, FaMinus } from 'react-icons/fa';
+import { FaPlus, FaMinus, FaSpinner } from 'react-icons/fa';
+import { AiOutlineReload } from 'react-icons/ai';
 import './StudentCard.scss'
 import SingleTextInput from '../singleTextInput/SinlgeTextInput';
 
 const StudentCard = ({ student }) => {
-    const { pic, firstname, lastname, company, skill, email, grades=[] } = student;
+    const { pic, firstname, lastname, company, skill, email } = student;
     const [showGrades, setShowGrades] = useState(false)
     const [tags, setTags] = useState([])
     const [tag, setTag] = useState('')
+    const [gradesLoading, setGradesLoading] = useState(false)
+
+    const [grades, setGrades] = useState([]);
 
     const calculateAverage = (grades) => {
-        return grades.reduce((acc, cur) => acc + Number(cur), 0) / grades.length
+        return grades.reduce((acc, cur) => acc + Number(cur.grade), 0) / grades.length
     }
-    const toggleGrades = e => {
-        e.stopPropagation(); //don't propagate down. 
+
+    const hideGrades = e => {
+        e.stopPropagation();
         e.preventDefault();
-        setShowGrades(!showGrades)
+        setShowGrades(false);
     }
+
+    const fetchAndShowGrades = e => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        //do we alredy have grades?
+        if (grades.length) {
+            setShowGrades(true)
+        } else {
+            setGradesLoading(true)
+
+            const url = 'http://localhost:3003/students/1/grades'
+
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    setGrades(data)
+                    setGradesLoading(false)
+                })
+
+        }
+    }
+
+    useEffect(() => {
+        if (grades.length)
+            setShowGrades(!showGrades)
+    }, [grades])
+
 
     return (
         <div className="studentCard">
@@ -41,36 +74,38 @@ const StudentCard = ({ student }) => {
                     <div className="studentCard__infoLine">
                         Skill : {skill}
                     </div>
-                    <div className="studentCard__infoLine">
-                        Average: {calculateAverage(grades)}%
-                    </div>
+
 
                     <div className="studentCard__gradesList" style={{ "display": showGrades ? "block" : "none" }}>
+                        <div className="studentCard__gradeAverage">
+                            Average: {calculateAverage(grades)}%
+                        </div>
                         {grades.map((grade, index) => {
-                            return <div key={index}><span>Test {index + 1}:</span> <span>{grade}%</span></div>
+                            return <div key={index}><span>Test {index + 1}:</span> <span>{grade.grade}%</span></div>
                         })}
                     </div>
 
-                    
+
 
                 </div>
 
                 <div className="studentCard__toggleIcons">
-                    {!showGrades ? <FaPlus className="studentCard__toggleIcons__toggleIcon" size='1.7em' onClick={(e) => toggleGrades(e)} />
-                        : <FaMinus size='1.7em' className="studentCard__toggleIcons__toggleIcon" onClick={(e) => toggleGrades(e)} />}
+                    {gradesLoading && <AiOutlineReload className="studentCard__toggleIcons__toggleIcon-spinning" size='1.7em' />}
+                    {(!showGrades && !gradesLoading) && <FaPlus className="studentCard__toggleIcons__toggleIcon" size='1.7em' onClick={(e) => fetchAndShowGrades(e)} />}
+                    {(showGrades && !gradesLoading) && <FaMinus size='1.7em' className="studentCard__toggleIcons__toggleIcon" onClick={(e) => hideGrades(e)} />}
                 </div>
             </Link>
             <div className="studentCard__tagCollection">
-                        <div className="studentCard__tags">
-                            {tags.map((tag, index)=>{
-                                return <span className="studentCard__tag" key={tag + index}>{tag}</span>
-                            })}
-                        </div>
-                        <div className="studentCard__tagInput">
-                            <SingleTextInput onSubmit={setTags} collection={tags} searchTerm={tag} setSearchTerm={setTag} placeholder="Add a tag" width="28%"/>
-                        </div>
+                <div className="studentCard__tags">
+                    {tags.map((tag, index) => {
+                        return <span className="studentCard__tag" key={tag + index}>{tag}</span>
+                    })}
+                </div>
+                <div className="studentCard__tagInput">
+                    <SingleTextInput onSubmit={setTags} collection={tags} searchTerm={tag} setSearchTerm={setTag} placeholder="Add a tag" width="28%" />
+                </div>
 
-                    </div>
+            </div>
 
         </div>
     );
