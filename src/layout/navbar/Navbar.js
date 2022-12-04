542185e6f61670f9128f69ff3f5b156e9255dcdb
Shoaib Dar <SDAR30@gmail.com>
2022-12-04 15:33:26 -0500

    fixed mobile styling menu items for navbar

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -1,20 +1,50 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.scss'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
-function Navbar(props) {
-    const [active, setActive] = useState(false)
+
+import './Navbar.scss';
+
+function Navbar() {
+
+    let navigate = useNavigate();
+
+    const [showNavbarItems, setShowNavbarItems] = useState(false);
+
+    useEffect(() => {
+        if (showNavbarItems) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+    }, [showNavbarItems]);
+
+    const toggleMenuItems = () => {
+        setShowNavbarItems(!showNavbarItems);
+    }
+
+    const handleNavigation = (e) => {
+        // close menu
+        setShowNavbarItems(false);
+        // navigate user to correct path
+        navigate(e.target.className.split(' ')[1]);
+    }
 
     return (
         <div className="navbar">
-            <div className='navbar__links'>
-                <div className="navbar__logo"> <Link to="/">Student App</Link ></div>
-                <div className={active ? "navbar__menuItems active" : "navbar__menuItems"}>
-                    <li className="navbar__menuItem"> <Link to="/">Students</Link ></li>
-                    <li className="navbar__menuItem"><Link to="/about">About</Link></li>
-                    <li className="navbar__menuItem"><Link to="/contact">Contact</Link></li>
+            <div className="navbar__overlay" style={{ "display": showNavbarItems ? "block" : "none" }}
+                onClick={() => setShowNavbarItems(false)}>  </div>
+            <div className="navbar__links">
+                <div className="navbar__logo /" onClick={handleNavigation} id='/' goto="/about">
+                    Student App
                 </div>
-                <div className="navbar__toggleIcon" onClick={() => setActive(!active)}>=</div>
+                <div className={showNavbarItems ? "navbar__menuItems-active" : "navbar__menuItems"}>
+                    <li className="navbar__menuItem /" onClick={handleNavigation}>  Students  </li>
+                    <li className="navbar__menuItem /about" onClick={handleNavigation}> About </li>
+                    <li className="navbar__menuItem /contact" onClick={handleNavigation}> Contact </li>
+                </div>
+                <div className="navbar__toggleIcon" onClick={toggleMenuItems}>
+                    =
+                </div>
             </div>
         </div>
     );
